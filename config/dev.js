7fff2090c9c486404f1152783f3aae48ff0ad44a
Ankit Kumar <104667338+CodeAnk2829@users.noreply.github.com>
2023-07-17 14:23:33 +0530

    Initial commits

---
+++ config/dev.js
@@ -0,0 +1,9 @@
+module.exports={
+    MongoDB: 'mongodb://127.0.0.1:27017/server',
+    FacebookAppID: '',
+    FacebookAppSecret:'',
+    GoogleClientID: '',
+    GoogleClientSecret: '',
+    AWSAccessKeyID: '',
+    AWSAccessKeySecret: ''
+};
